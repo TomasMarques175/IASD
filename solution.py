7d9dc1e5e7fd10a36341dcd9bdc2c2d742b95b96
Tomás Marques <tomas.luis.marques@gmail.com>
2024-09-20 11:46:45 +0100

    inicialização

---
+++ solution.py
@@ -0,0 +1,14 @@
+import search
+
+class BAProblem(search.Problem):
+    def __init__(self):
+        self.initial = None
+
+    def load(self, fh):
+        pass
+
+    def cost(self, sol):
+        pass
+
+    def check(self, sol):
+        pass
